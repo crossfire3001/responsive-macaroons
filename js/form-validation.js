41546b1a2501a4c4d87fabb22ef0ae0f13173f65
crossfire3001 <obiwan2035@mail.ru>
2023-10-13 18:20:33 +0500

    jquery validation

---
+++ js/form-validation.js
@@ -0,0 +1,25 @@
+$("#submit").click(function (e) {
+    e.preventDefault();
+        let foodName = $("#foodName");
+        let name = $("#name");
+        let phone = $("#phone");
+        let hasError = false;
+        $(".error-input").hide();
+
+        if (!foodName.val()) {
+            foodName.next().show();
+            hasError = true;
+        }
+        if (!name.val()) {
+        name.next().show();
+        hasError = true;
+        }
+        if (!phone.val()) {
+        phone.next().show();
+        hasError = true;
+        }
+
+        if(!hasError) {
+            // ajax
+        }
+});
